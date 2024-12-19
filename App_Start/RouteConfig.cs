0211a08f5a4bfbb88831ff3ef12172e3765e22f4
admin <admin@DESKTOP-63NSF9A>
2024-12-19 01:36:17 -0800

    Add project files.

---
+++ App_Start/RouteConfig.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace DATEnglishCenter
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+     name: "DATRoute",
+     url: "DAT/{action}/{id}",
+     defaults: new { controller = "DAT", action = "Index", id = UrlParameter.Optional }
+ );
+
+        }
+    }
+}
